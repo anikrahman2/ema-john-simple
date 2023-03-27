c02a394da3041bf2009a3c305d59885debaa2aea
Anik Rahman <anikrahman247@gmail.com>
2023-03-27 15:57:48 +0600

    Product card added

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,20 @@
+import './Product.css';
+import React from 'react';
+
+const Product = (props) => {
+  const {name, price, img, ratings, seller, stock, quantity} = props.product;
+  return (
+    <div className='product'>
+      <img src={img} alt="" />
+      <div className='product-info'>
+        <h6 className='product-name'>{name}</h6>
+        <p>Price: ${price}</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Rating: {ratings} Stars</p>
+      </div>
+      <button className='btn-cart'>Add To Cart</button>
+    </div>
+  );
+};
+
+export default Product;
